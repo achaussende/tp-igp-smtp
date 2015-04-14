058d18d91315b5c63cae5721ac0870afd2901107
Slashgear <antoine395.caron@laposte.net>
2015-04-14 19:38:40 +0200

    ACN: Fix StateMail

--- Client/src/main/java/com.polytech4a.smtp.client/core/State/StateMail.java
+++ Client/src/main/java/com.polytech4a.smtp.client/core/State/StateMail.java
@@ -4,11 +4,17 @@ import com.polytech4a.smtp.client.core.Mail;
 import com.polytech4a.smtp.messages.SMTPMessage;
 import com.polytech4a.smtp.messages.exceptions.MalformedEmailException;
 import com.polytech4a.smtp.messages.textheader.client.RCPTTO;
+import org.apache.log4j.Logger;
 
 /**
  * Created by Pierre on 08/04/2015.
  */
 public class StateMail extends State{
+
+    /**
+     * Logger.
+     */
+    private static final Logger logger=Logger.getLogger(StateMail.class);
 
     public StateMail(Mail mailToSend) throws MalformedEmailException {
         super(mailToSend);
@@ -17,10 +23,20 @@ public class StateMail extends State{
     @Override
     public boolean analyze(String message) {
         if(message == null && !SMTPMessage.matches(SMTPMessage.OK, message)){
-            incrementNbTry();
-            return false;
+            try {
+                this.setMsgToSend(new RCPTTO(mailToSend.getReceivers().get(0)).toString());
+                this.setNextState(new StateRcpt(mailToSend));
+            } catch (MalformedEmailException e) {
+                logger.error(e);
+            }
+            return true;
+        }else{
+            if(SMTPMessage.matches(SMTPMessage.BAD_SEQUENCE_OF_COMMANDS, message)){
+                this.setMsgToSend(SMTPMessage.QUIT.toString());
+                this.setNextState(new StateQuit(mailToSend));
+                return true;
+            }
         }
-        this.setNextState(new StateRcpt(mailToSend));
-        return true;
+        return false;
     }
 }
