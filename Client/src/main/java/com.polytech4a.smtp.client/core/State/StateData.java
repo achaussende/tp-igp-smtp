04b90d0f09b0d90556460b2a671f2b961b713f19
PReynaud <pierre.reynaud@hotmail.fr>
2015-04-17 17:04:09 +0200

    Integration of the mail manager

--- Client/src/main/java/com.polytech4a.smtp.client/core/State/StateData.java
+++ Client/src/main/java/com.polytech4a.smtp.client/core/State/StateData.java
@@ -1,12 +1,17 @@
 package com.polytech4a.smtp.client.core.State;
 
 import com.polytech4a.smtp.client.core.Mail;
+import com.polytech4a.smtp.mailmanager.FacadeClient;
+import com.polytech4a.smtp.mailmanager.exceptions.MailManagerException;
+import com.polytech4a.smtp.mailmanager.exceptions.MalFormedMailException;
 import com.polytech4a.smtp.messages.SMTPMessage;
+import org.apache.log4j.Logger;
 
 /**
  * Created by Pierre on 14/04/2015.
  */
 public class StateData extends State {
+    private static final Logger logger = Logger.getLogger(StateData.class);
 
     public StateData(Mail mailToSend) {
         super(mailToSend);
@@ -22,9 +27,22 @@ public class StateData extends State {
         }
 
         if(SMTPMessage.matches(SMTPMessage.START_MAIL_INPUT, message)){
-            this.setMsgToSend(this.mailToSend.getMailToSend());
-            this.setNextState(new StateDataConfirm(mailToSend));
-            return true;
+            try {
+                String mail = FacadeClient.createMail(mailToSend.getUser(), mailToSend.getReceivers().get(0), mailToSend.getMailToSend(), mailToSend.getObject());
+                FacadeClient.saveMail(mail, mailToSend.getUser() ,"OUTPUT/Client/");
+                this.setMsgToSend(mail);
+                this.setNextState(new StateDataConfirm(mailToSend));
+                return true;
+            } catch (MalFormedMailException e) {
+                logger.error("Wrong message to send", e);
+                this.setMsgToSend(SMTPMessage.QUIT.toString());
+                this.setNextState(new StateQuit(mailToSend));
+                return true;
+            } catch (MailManagerException e) {
+                logger.warn("Error in the mail manager", e);
+                this.setNextState(new StateDataConfirm(mailToSend));
+                return true;
+            }
         }
         else
             return false;
